48dcca0b4466344dc584dde0e96acd1319a1e3cc
cllcnkrt <cllcnkrt@outlook.com>
2021-08-28 12:58:03 +0300

    webcam added and connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './components/WebcamCapture/WebcamCapture';
 
 function App() {
   return (
     <div className="app">
-      <h1>snapchat</h1>
+   
+      <WebcamCapture />
     </div>
   );
 }
---
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -0,0 +1,43 @@
+import React, {useCallback, useRef} from "react";
+import Webcam from "react-webcam";
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+const videoConstraints = {
+  width: 250,
+  height: 400,
+  facingMode: "user",
+};
+
+function WebcamCapture() {
+
+  const webcamRef = useRef(null);
+   
+    const capture = useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+
+        },
+        [webcamRef],
+    )
+  return (
+    <div className="webcamCapture">
+      <Webcam 
+      audio={false}
+      height={videoConstraints.height}
+      ref={webcamRef}
+      screenshotFormat="image/jpeg"
+      width={videoConstraints.width}
+      videoConstraints={videoConstraints}
+      />
+
+    <RadioButtonUncheckedIcon 
+    className="webcamCapture__button"
+    onClick={capture}
+    fontSize="large"
+    />
+
+
+    </div>
+  );
+}
+
+export default WebcamCapture;

4dbd8efe3a6abc4af70f9d073e32c65790574df4
cllcnkrt <cllcnkrt@outlook.com>
2021-08-28 13:37:19 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from './components/WebcamCapture/WebcamCapture';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-   
-      <WebcamCapture />
+   <Router>
+        <Switch>
+
+          <Route exact path="/test">
+          <h1>Heyyy</h1>
+          </Route>
+
+          <Route exact path="/">
+          <WebcamCapture />
+          </Route>
+
+        </Switch>
+    
+    </Router>
+  );
+
+
+
+    
     </div>
   );
 }
--- src/components/WebcamCapture/WebcamCapture.js
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -1,6 +1,8 @@
-import React, {useCallback, useRef} from "react";
+import React, { useCallback, useRef } from "react";
 import Webcam from "react-webcam";
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import { useDispatch } from "react-redux";
+import { setCameraImage } from "../../features/cameraSlice";
 const videoConstraints = {
   width: 250,
   height: 400,
@@ -8,34 +10,30 @@ const videoConstraints = {
 };
 
 function WebcamCapture() {
-
   const webcamRef = useRef(null);
-   
-    const capture = useCallback(
-        () => {
-            const imageSrc = webcamRef.current.getScreenshot();
+  const dispatch = useDispatch()
+  
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    dispatch(setCameraImage(imageSrc))
+  }, [webcamRef]);
 
-        },
-        [webcamRef],
-    )
   return (
     <div className="webcamCapture">
-      <Webcam 
-      audio={false}
-      height={videoConstraints.height}
-      ref={webcamRef}
-      screenshotFormat="image/jpeg"
-      width={videoConstraints.width}
-      videoConstraints={videoConstraints}
+      <Webcam
+        audio={false}
+        height={videoConstraints.height}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={videoConstraints.width}
+        videoConstraints={videoConstraints}
       />
 
-    <RadioButtonUncheckedIcon 
-    className="webcamCapture__button"
-    onClick={capture}
-    fontSize="large"
-    />
-
-
+      <RadioButtonUncheckedIcon
+        className="webcamCapture__button"
+        onClick={capture}
+        fontSize="large"
+      />
     </div>
   );
 }
